c1f8db959fc247213dfed247b9a85253de1dadfb
Nicolás Salas V <nikosalas@gmail.com>
2019-12-29 03:52:17 -0300

    chore: Add devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,9 @@
+# Settings specified here will take precedence over those in config/application.rb.
+
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  # Devise: change me!!!!
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
